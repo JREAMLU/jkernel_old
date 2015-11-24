f198040590178e98ca0178932c57318e7549bc94
jream <lujunjie@yunjiazheng.com>
2015-11-24 15:01:20 +0800

    i18n

--- src/base/cores/languages/en_US.go
+++
@@ -1,8 +0,0 @@
-package languages
-
-var en_US = map[string]string{
-	"SUCCESS":       "SUCCESS",
-	"PARAMSILLEGAL": "PARAMS ILLEGAL",
-	"LOGICILLEGAL":  "LOGIC ILLEGAL",
-	"SYSTEMILLEGAL": "SYSTEM ILLEGAL",
-}
--- src/base/cores/languages/zh_CN.go
+++
@@ -1,8 +0,0 @@
-package languages
-
-var zh_CN = map[string]string{
-	"SUCCESS":       "成功",
-	"PARAMSILLEGAL": "参数错误",
-	"LOGICILLEGAL":  "逻辑错误",
-	"SYSTEMILLEGAL": "系统错误",
-}
